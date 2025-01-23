30b3999b99545f686badad70ac3de84315d7f0a5
Tsikairis Nikolaos <tsiakiris.ni@gmail.com>
2025-01-23 21:04:26 +0200

    next config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-// import { geistSans, geistMono } from 'some-module'; // Comment out or remove this line if not needed
 
 export const metadata: Metadata = {
   title: "Reservation System",
